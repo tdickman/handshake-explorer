ff693212588d0af75786e79961cf61f7d219648e
Tom Dickman <tdickman@gmail.com>
2018-08-12 02:28:44 -0500

    Removed admin since it is not used

--- hsdexplorer/hsdexplorer/urls.py
+++ hsdexplorer/hsdexplorer/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('', include('explorer.urls')),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
 ]
