310893688f5275a608cec19840eb9de0acccadf8
Tom Dickman <tdickman@gmail.com>
2018-08-23 14:39:31 -0500

    Added missing import

--- hsdexplorer/explorer/tasks.py
+++ hsdexplorer/explorer/tasks.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from django.conf import settings
+import json
 import redis
 
 import explorer.history.write as hwrite
